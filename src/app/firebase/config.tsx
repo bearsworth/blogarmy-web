ad0d1dcac39e48fc3cd52b37b556291ba382805b
markdev <mark@aimrd.com>
2025-09-19 15:30:54 -0700

    update with new auth client

--- src/app/firebase/config.tsx
+++ src/app/firebase/config.tsx
@@ -8,13 +8,13 @@ import { getAuth } from "@firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB0eNVmRakprDjyk9zAHJUk2c9pyLYG54A",
-  authDomain: "blogarmy-1b50f.firebaseapp.com",
-  projectId: "blogarmy-1b50f",
-  storageBucket: "blogarmy-1b50f.firebasestorage.app",
-  messagingSenderId: "426432998696",
-  appId: "1:426432998696:web:d403ce1c01c241ebee341d",
-  measurementId: "G-6GJW268VDW",
+  apiKey: "AIzaSyCCV2nokZ_WYclfwopeeeZRu6fZ_JF6PXw",
+  authDomain: "app.blogarmy.com",
+  projectId: "blogarmy-82425",
+  storageBucket: "blogarmy-82425.firebasestorage.app",
+  messagingSenderId: "17635041503",
+  appId: "1:17635041503:web:77d13d3c9adc35cc7e096a",
+  measurementId: "G-XEE627NS93",
 };
 
 // Initialize Firebase
